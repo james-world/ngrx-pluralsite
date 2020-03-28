2d0e2b1e655ff02283123b242259c3f6e2a4d1dd
James World <james-world@users.noreply.github.com>
2020-03-28 10:26:52 +0000

    Subscribe to product slice (no unsub yet)
    Subscribe to observable for product slice in redux store
    Update displayCode
    Note we are not managing unsubscribe from this subscription yet

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -4,7 +4,7 @@ import { Subscription } from 'rxjs';
 
 import { Product } from '../product';
 import { ProductService } from '../product.service';
-import { Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
 
 @Component({
   selector: 'pm-product-list',
@@ -34,6 +34,15 @@ export class ProductListComponent implements OnInit, OnDestroy {
     this.productService.getProducts().subscribe({
       next: (products: Product[]) => this.products = products,
       error: (err: any) => this.errorMessage = err.error
+    });
+
+    // TODO: Unsubscribe
+    this.store.pipe(select('products')).subscribe({
+      next: products => {
+        if (products) {
+          this.displayCode = products.showProductCode;
+        }
+      }
     });
   }
 
