7d1648067e4a7f5ec301fa15ecf520a0220e6117
James World <james-world@users.noreply.github.com>
2020-03-28 10:12:06 +0000

    Add product reducer to toggle product code display

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -7,18 +7,19 @@ import { ProductShellComponent } from './product-shell/product-shell.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
 
+/* NgRx */
+import { StoreModule } from '@ngrx/store';
+import { reducer } from './state/product.reducer';
+
 const productRoutes: Routes = [
   { path: '', component: ProductShellComponent }
 ];
-
-/* NgRx */
-import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   imports: [
     SharedModule,
     RouterModule.forChild(productRoutes),
-    StoreModule.forFeature('products', {}),
+    StoreModule.forFeature('products', reducer),
   ],
   declarations: [
     ProductShellComponent,
