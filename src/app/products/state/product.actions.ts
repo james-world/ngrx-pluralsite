53eb141a617ddd66a0f5245f2d30e8af72b8039d
James World <james-world@users.noreply.github.com>
2020-03-29 10:41:10 +0100

    Add Load actions

--- src/app/products/state/product.actions.ts
+++ src/app/products/state/product.actions.ts
@@ -5,7 +5,10 @@ export enum ProductActionTypes {
     ToggleProductCode = '[Product] Toggle Product Code',
     SetCurrentProduct = '[Product] Set Current Product',
     ClearCurrentProduct = '[Product] Clear Current Product',
-    InitializeCurrentProduct = '[Product] Initialize Current Product'
+    InitializeCurrentProduct = '[Product] Initialize Current Product',
+    Load = '[Product] Load',
+    LoadSuccess = '[Product] Load Success',
+    LoadFail = '[Product] Load Fail'
 }
 
 export class ToggleProductCode implements Action {
@@ -22,18 +25,33 @@ export class SetCurrentProduct implements Action {
 
 export class ClearCurrentProduct implements Action {
     readonly type = ProductActionTypes.ClearCurrentProduct;
-
-    constructor() {}
 }
 
 export class InitializeCurrentProduct implements Action {
     readonly type = ProductActionTypes.InitializeCurrentProduct;
+}
 
-    constructor() {}
+export class Load implements Action {
+    readonly type = ProductActionTypes.Load;
+}
+
+export class LoadSuccess implements Action {
+    readonly type = ProductActionTypes.LoadSuccess;
+
+    constructor(public payload: Product[]) {}
+}
+
+export class LoadFail implements Action {
+    readonly type = ProductActionTypes.LoadFail;
+
+    constructor(public payload: string) {}
 }
 
 export type ProductActions =
     | ToggleProductCode
     | SetCurrentProduct
     | ClearCurrentProduct
-    | InitializeCurrentProduct;
+    | InitializeCurrentProduct
+    | Load
+    | LoadSuccess
+    | LoadFail;
